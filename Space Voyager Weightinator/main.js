555dd60ef99ac7a7c265d8d98d665ff176acfa90
Justin Acosta <150075360+JT-Acosta@users.noreply.github.com>
2023-12-08 13:57:17 -0800

    Update main.js

--- Space Voyager Weightinator/main.js
+++ Space Voyager Weightinator/main.js
@@ -1,24 +1,3 @@
-
-// We are going to solve this by breaking the problem into three parts.
-// Programmers like automating things, we'll populate the HTML drop down options using code,
-// instead of having to type out all the values.
-// Create a function that does the some math and gives us the new weight.
-// Then create a function that responds when the user clicks on the button.
-
-// 1. Populate the dropdown element with the data found in the planets array.
-// The value of each option should be the planet's name.
-// Use the following built-in methods:
-// `.forEach` `document.createElement` `document.getElementById` `.appendChild`
-// 2. Write the code to return the correct weight
-// 3. Declare a variable called userWeight and assign the value of the user's weight.
-// 4. Delcare a variable called planetName and assign the name of the selected planet from the drop down.
-// 5. Declare a variable called result and assign the value of the new calculated weight.
-// 6. Write code to display the message shown in the screenshot.
-// 7. Set the #calculate-button element's onclick method to use the handleClickEvent function.
-// 8. Make it look nice by attaching  a style.css file to your index.html and writing some basic styling,
-// feel free to add classes and id's to the HTML elements as you need,
-// import.a google font and use it for some or all of the text on your page
-// 8. Reverse the drop down order so that the sun is first.
 const planets = [
   ['Sun', 27.9],
   ['Mercury', 0.377],
